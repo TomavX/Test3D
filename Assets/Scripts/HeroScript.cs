f840e81d36127acf2c48eb35cb497051665f8c18
TomavX <acwokup@gmail.com>
2017-03-12 02:00:21 +0100

    Initial commit of Unity 3D learning project.

---
+++ Assets/Scripts/HeroScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroScript : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float v = Input.GetAxis("Vertical");
+        anim.SetFloat("speed",Input.GetAxis("Vertical"));
+        transform.Translate(new Vector3(0, 0, .06f * -v));
+        anim.SetBool("papa", Input.GetKey("space"));
+	}
+}
